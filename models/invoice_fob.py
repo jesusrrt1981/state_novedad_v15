8cb36aebfd244adbeb98af3e32d3b7f73e2772af
jesus rodriguez <jesusrrt1981@gmail.com>
2023-09-12 15:37:05 -0400

    ajuste

--- models/invoice_fob.py
+++ models/invoice_fob.py
@@ -5,11 +5,13 @@ class Company(models.Model):
     _inherit="hr.news"
 
     
-    @api.model
+
     def cancel_pay(self):
         for line in self:
-            if line.state=="done":
-                line.state="approved"
+            if line.state=="draft":
+                line.button_to_approve()
+                line.button_approved()
+
             
 
    
